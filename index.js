9e6a5d2104a436e83bbbb6d9e393c41c87695f39
Oteis <vitineitor@gmail.com>
2025-10-08 18:20:53 +0200

    Fix estructura para Vercel

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from "express";
-
 const app = express();
 
-// servir archivos estáticos (tu frontend)
 app.use(express.static("public"));
-
-// ruta principal
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
